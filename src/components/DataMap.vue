163de38418715dcaf7df4b75852dc6017371e7b4
Stephanie McCumsey <smccumsey@gmail.com>
2018-05-02 23:02:17 -0600

    gotta add the slider!

--- src/components/DataMap.vue
+++ src/components/DataMap.vue
@@ -9,17 +9,27 @@ import L from 'leaflet';
 export default {
   data() {
     return {
-      crimeData: null,
       map: null,
-      uniqueOffenses: null,
+      crimeData: null,
       layers: {},
+      dateRange: [],
     }
   },
   methods: {
-    getUnique(dataset, prop) {
+    sortByDate(dataset, dateKey) {
+      return dataset.sort(function(a, b){
+        var keyA = new Date(a[dateKey]),
+            keyB = new Date(b[dateKey]);
+        // Compare the 2 dates
+        if(keyA < keyB) return -1;
+        if(keyA > keyB) return 1;
+        return 0;
+      });
+    },
+    getUnique(dataset, key) {
       // get offense categories
       const uniqueArr = dataset.reduce((acc, elem) => {
-        const value = elem[prop];
+        const value = elem[key];
           if (acc.indexOf(value) === -1){
            acc.push(value);
           }
@@ -34,45 +44,51 @@ export default {
           data.forEach(d => {
             d.REPORTDATE = parseTime(d.REPORTDATE.split(' ')[0]);
           })
-          const isObject = (t) => { return Object.prototype.toString.call(t); };
-          // remove data that doesn't have location coordinates
-          return resolve(data.filter(x => isObject(x.location)));
+          // filter out data that doesn't have location coordinates
+          const filteredData = data.filter(x => x.location !== 'timeout');
+          return resolve(filteredData);
         }).catch((err) => {
           reject(err);
         });
       });
     },
-    setup() {
-      var map = L.map('map').setView([40.025647, -105.257869], 12.14);
+    initMap() {
+      this.map = L.map('map').setView([40.025647, -105.257869], 12.14);
       //add choice base tiles - background map of Lyon
       L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
+      }).addTo(this.map);
 
-      d3.json('static/data/BoulderCityLimits.geojson')
+      d3.json('static/data/Subcomm.GeoJSON')
         .then((cityData) => {
+          const subcoms = cityData.features.map(x => x.properties.SUBCOM);
+          const color = d3.scaleOrdinal()
+            .domain(subcoms)
+            .range(d3.quantize(t => d3.interpolateGreys(t * .7 + 0.5), subcoms.length).reverse())
+
           L.geoJSON(cityData, {
               style: function (feature) {
-                return { weight: 1, color: '#808080' };
+                return { weight: 2, color: '#808080' };
                   // return {color: feature.properties.color};
               }
-          }).addTo(map);
+          }).bindTooltip(function (layer) {
+              return layer.feature.properties.SUBCOM;
+          }).addTo(this.map);
         });
-
+    },
+    setup() {
       this.loadData().then((res) => {
         // map.on('load', function () {
           const organizedCrime = {};
-          const crimeData = res;
-          const uniqueOffenses = this.getUnique(crimeData, 'OFFENSE');
-          // const colors = d3.scaleOrdinal.range(["#A07A19", "#AC30C0", "#EB9A72", "#BA86F5", "#EA22A8"]);
+          this.crimeData = this.sortByDate(res, 'REPORTDATE');
+          this.dateRange = d3.extent(this.crimeData, d => d.REPORTDATE)
+
+          const uniqueOffenses = this.getUnique(this.crimeData, 'OFFENSE');
           const color = d3.scaleOrdinal()
             .domain(uniqueOffenses)
-            .range(d3.quantize(t => d3.interpolateSpectral(t * 0.99 + 0.1), uniqueOffenses.length).reverse())
+            .range(d3.quantize(t => d3.interpolateWarm(t * 0.8 + 0.3), uniqueOffenses.length))
           // const spectrum = d3.scaleOrdinal(d3.schemeRdYlGn[uniqueOffenses.length]);
-          crimeData.forEach((x) => {
-            // create a HTML element for each feature
-            var el = document.createElement('div');
-            el.className = 'marker';
+          this.crimeData.forEach((x) => {
             const {lng, lat} = x.location;
             if (lng && lat) {
               let circle = L.circleMarker([lat, lng], {
@@ -83,22 +99,27 @@ export default {
               });
               organizedCrime[x.OFFENSE] = organizedCrime[x.OFFENSE] || [];
               organizedCrime[x.OFFENSE].push(circle);
-              // circle.addTo(map);
             }
           })
           console.log(organizedCrime);
           Object.keys(organizedCrime).forEach((key) => {
             this.layers[key] = L.layerGroup(organizedCrime[key])
               .on('click', function() { L.Layer.remove() })
-              .addTo(map);
+              .addTo(this.map);
           })
-          L.control.layers(this.layers).addTo(map);
+          L.control.layers(this.layers, null, {
+            collapsed: false,
+            sortLayers: true,
+            sortFunction: (layerA, layerB, nameA, nameB)  => {
+              return organizedCrime[nameB].length - organizedCrime[nameA].length;
+            }
+          }).addTo(this.map);
         // })
       });
     }
   },
   mounted() {
-    this.loadData();
+    this.initMap();
     this.setup();
   }
 }
